dad5fbff1e8a145d59c8643085120cc6b79a8775
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-23 10:29:02 -0300

    add stock-ui

---
+++ stock-ui/src/main/java/com/github/fabiomqs/stockui/StockUiApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs.stockui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockUiApplication.class, args);
+	}
+
+}
